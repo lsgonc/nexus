5f2c258937bf78ff4979d16e1e07e0d829d8cf65
Lucas Sciarra <lucas.goncalves@vonbraunlabs.com.br>
2024-12-18 14:42:21 -0300

    Criação inicial do projeto e estruturação básica do menu de navegação

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById('menu-button');
+const menuItems = document.getElementById('menu-items');
+
+menuButton.addEventListener('click', () => {
+    menuItems.classList.toggle('hidden');
+});
